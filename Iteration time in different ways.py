f0578c4cb78ef6ab14003a6c7359404dacacaa9a
Vihagle <1002460406@qq.com>
2021-02-23 20:50:04 +0800

    Update and rename Iterate in different ways.py to Iteration time in different ways.py

--- Iteration time in different ways.py
+++ Iteration time in different ways.py
@@ -7,6 +7,7 @@ def time_count(func):
         print(f'It costs {end-start}s')
     return wrapper
 
+#Method 1
 @time_count
 def iterate_1(var):
     ls = []
@@ -14,7 +15,19 @@ def iterate_1(var):
         ls.append(var[i].upper())
     print(ls)
 
+#Method 2    
 @time_count
 def iterate_2(var):
     res = list(map(lambda x:x.upper(),var))
     print(res)
+
+#Method 3
+@time_count
+def iterate_3(var):
+    res_1 = [fruit.upper() for fruit in var]
+    print(res_1)
+
+var = ['apple','pear','orange','banana']
+iterate_1(var)
+iterate_2(var)
+iterate_3(var)
